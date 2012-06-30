05dafc289b9e774574efd01320331526ff4cb3d6
Chris Parsons + Kevin Rutherford + Steve Tooke + Jim Weirich + Matt Wynne <steve+cp+kr+st+jw+mw@heavi.es>
2012-06-30 16:30:51 +0100

    Refactor, simplifying code in rename temp

--- lib/raffle/refactorings/rename_temp.rb
+++ lib/raffle/refactorings/rename_temp.rb
@@ -2,21 +2,15 @@ module Raffle
   module Refactorings
     class RenameTemp
       def call(sexpr, original_name, new_name)
-        rename_temp(sexpr, original_name, new_name)
-      end
-
-      private
-
-      def rename_temp(sexpr, original_name, new_name)
         if sexpr.respond_to?(:each)
-          new = replace_ident(sexpr, original_name, new_name)
-          new.map { |child| rename_temp(child, original_name, new_name) }
+          new = transform(sexpr, original_name, new_name)
+          new.map { |child| call(child, original_name, new_name) }
         else
           sexpr
         end
       end
 
-      def replace_ident(node, original_name, new_name)
+      def transform(node, original_name, new_name)
         return node unless node[0] == :@ident && node[1] == original_name
         [:@ident, new_name, node[2]]
       end
