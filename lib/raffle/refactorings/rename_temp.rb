c93304565edd43908a55520bf7d566eb44e046bd
Matt Wynne <matt@mattwynne.net>
2012-07-01 23:17:49 +0100

    Refactor to use new transform method

--- lib/raffle/refactorings/rename_temp.rb
+++ lib/raffle/refactorings/rename_temp.rb
@@ -2,14 +2,18 @@ module Raffle
   module Refactorings
     class RenameTemp
       def call(sexpr, original_name, new_name)
-        return sexpr unless sexpr.respond_to?(:each)
-        new = transform(sexpr, original_name, new_name)
-        new.map { |child| call(child, original_name, new_name) }
+        transform(sexpr) do |node|
+          if node[0] == :@ident && node[1] == original_name
+            [:@ident, new_name, node[2]]
+          end
+        end
       end
 
-      def transform(node, original_name, new_name)
-        return node unless node[0] == :@ident && node[1] == original_name
-        [:@ident, new_name, node[2]]
+      def transform(node, &block)
+        return node unless node.respond_to?(:map)
+        block.call(node) || node.map do |child|
+          transform(child, &block)
+        end
       end
     end
   end
