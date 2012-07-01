7baac575b3006e1e7c5c91563ef66d464379ae12
Matt Wynne <matt@mattwynne.net>
2012-07-01 22:44:18 +0100

    Refactor RemoveUnusedTemp to use the same pattern

--- lib/raffle/refactorings/remove_unused_temp.rb
+++ lib/raffle/refactorings/remove_unused_temp.rb
@@ -3,19 +3,15 @@ module Raffle
   module Refactorings
     class RemoveUnusedTemp
       include Refactoring
+
       def call(sexpr, temp_name)
-        remove_node(sexpr, temp_name)
+        return sexpr unless sexpr.respond_to?(:each)
+        new = transform(sexpr, temp_name)
+        new.map { |child| call(child, temp_name) }
       end
 
-      private
-
-      def remove_node(sexpr, temp_name)
-        if sexpr.respond_to?(:each)
-          filtered_expressions = sexpr.reject { |child| assignment_with_name?(child, temp_name) }
-          filtered_expressions.map { |s| remove_node(s, temp_name) }
-        else
-          sexpr
-        end
+      def transform(node, temp_name)
+        node.reject { |child| assignment_with_name?(child, temp_name) }
       end
     end
   end
