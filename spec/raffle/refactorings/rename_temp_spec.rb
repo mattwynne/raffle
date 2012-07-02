33d41ce8ae98c64cfbf21dbcadaf3e15e663782d
Matt Wynne <matt@mattwynne.net>
2012-07-03 00:30:27 +0100

    Add a failing test for scope

--- spec/raffle/refactorings/rename_temp_spec.rb
+++ spec/raffle/refactorings/rename_temp_spec.rb
@@ -2,14 +2,18 @@ require 'ripper_helper'
 require_relative '../../../lib/raffle/refactorings/rename_temp'
 
 describe Raffle::Refactorings::RenameTemp do
-  it 'changes all occurrences of the temp name' do
+  it 'changes all occurrences of the temp name, but only within the given scope' do
     input = %{
       def foo
         fred = 45
         june = fred
       end
+
+      def bar
+        fred = "captain"
+      end
     }
-    output = refactor(input, 'fred', 'billy')
+    output = refactor(input, 'fred', 'billy', [3,0])
     output.should == "def foo\nbilly = 45\njune = billy\nend"
   end
 
