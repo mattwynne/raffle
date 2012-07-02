8a4051292d6b2bcc9d6f06f21c423968846b3874
Kevin Rutherford <kevin@rutherford-software.com>
2012-07-02 14:45:20 +0100

    Added some specs to find edge cases in InlineTemp

--- spec/raffle/refactorings/inline_temp_spec.rb
+++ spec/raffle/refactorings/inline_temp_spec.rb
@@ -2,24 +2,75 @@ require 'ripper_helper'
 require_relative '../../../lib/raffle/refactorings/inline_temp'
 
 describe Raffle::Refactorings::InlineTemp do
-  it 'returns an s-expression with the temp inlined' do
-    input = %{
-    def thing
-      fred = 35
-      june = fred
+
+  context 'when the temp can be safely inlined' do
+
+    it 'replaces all uses of the temp' do
+      input = '
+      def thing
+        fred = 35
+        june = fred
+        "I was #{fred} years old"
+      end
+      '
+      output = refactor(input, "fred")
+      output.should == 'def thing
+fred = 35
+june = 35
+"I was #{35} years old"
+end'
     end
-    }
-    output = refactor(input, "fred")
-    output.should == "def thing\nfred = 35\njune = 35\nend"
+
+    context 'when the temp uses an overloaded name' do
+      # e.g.
+      # def foo
+      #   name = "fred"
+      #   @names.each {|name| p name }
+      #   "#{name} ace"
+      # end
+      pending 'how do we specify which one to inline?'
+    end
+
+    context 'when the temp occurs in several methods' do
+      # e.g.
+      # def foo
+      #   name = "fred"
+      #   "#{name} ace"
+      # end
+      #
+      # def bar
+      #   name = "fred"
+      #   "#{name} ace"
+      # end
+      pending 'how do we specify which one to inline?'
+    end
+
   end
 
-  context 'when the temp is mutated' do
-    # e.g.
-    # def foo
-    #   name = "fred"
-    #   name << " is "
-    #   "#{name} ace"
-    # end
-    example 'what should we do?'
+  context 'when the temp cannot be safely inlined' do
+
+    context 'when the temp is mutated' do
+      # e.g.
+      # def foo
+      #   name = "fred"
+      #   name << " is "
+      #   "#{name} ace"
+      # end
+      it 'throws an error'
+    end
+
+    context 'when the temp is assigned twice' do
+      # e.g.
+      # def foo
+      #   name = "fred"
+      #   @lucy = name
+      #   name = 'bob'
+      #   "#{name} ace"
+      # end
+      it 'throws an error'
+    end
+
   end
+
 end
+
