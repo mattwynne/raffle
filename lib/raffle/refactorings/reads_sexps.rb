b625e8f3a7a3a574666f724aa76bdd441abc403c
Matt Wynne <matt@mattwynne.net>
2012-07-03 00:16:39 +0100

    Factor out duplication in sexp methods

--- lib/raffle/refactorings/reads_sexps.rb
+++ lib/raffle/refactorings/reads_sexps.rb
@@ -6,16 +6,14 @@ module Raffle
         (
           sexp[0] == :assign &&
           sexp[1][0] == :var_field &&
-          sexp[1][1][0] == :@ident &&
-          sexp[1][1][1] == name
+          ident?(sexp[1][1], name)
         )
       end
 
       def var_ref?(sexp, name)
         (
           sexp[0] == :var_ref &&
-          sexp[1][0] == :@ident &&
-          sexp[1][1] == name
+          ident?(sexp[1], name)
         )
       end
 
