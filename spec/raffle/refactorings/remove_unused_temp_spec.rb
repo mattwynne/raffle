4ed7d66e0acf659b2bcd5b1cfd0254e5d589faa3
Chris Parsons <chris@thinkcodelearn.com>
2012-07-02 14:11:35 +0100

    Temp to remove not found already works, add spec

--- spec/raffle/refactorings/remove_unused_temp_spec.rb
+++ spec/raffle/refactorings/remove_unused_temp_spec.rb
@@ -2,18 +2,23 @@ require 'ripper_helper'
 require_relative '../../../lib/raffle/refactorings/remove_unused_temp'
 
 describe Raffle::Refactorings::RemoveUnusedTemp do
-  it 'returns an s-expression with the temp removed' do
-    input = %{
-    def thing
-      fred = 35
-      42
-    end
+  let(:input) do
+    %{
+      def thing
+        fred = 35
+        42
+      end
     }
+  end
+
+  it 'returns an s-expression with the temp removed' do
     refactor(input, 'fred').should == "def thing\n42\nend"
   end
 
   context 'when the temp is not found' do
-    it 'returns the s-expression unchanged'
+    it 'returns the s-expression unchanged' do
+      refactor(input, 'jim').should == "def thing\nfred = 35\n42\nend"
+    end
   end
 
   context 'when the temp is used' do
