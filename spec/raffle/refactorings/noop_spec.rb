6f479abceb374d9c7c763d9a271bb4146bd54f09
Matt Wynne <matt@mattwynne.net>
2012-07-03 16:00:40 +0100

    Add a spec for brackets in the noop

--- spec/raffle/refactorings/noop_spec.rb
+++ spec/raffle/refactorings/noop_spec.rb
@@ -2,8 +2,14 @@ require 'ripper_helper'
 require 'raffle/refactorings/noop'
 
 describe Raffle::Refactorings::Noop do
+
+  def round_trip(input)
+    # sourcerer skips the last newline, but it makes our tests more readable
+    refactor(input).should == input.split("\n").join("\n")
+  end
+
   it 'reformats all code to two space indentation' do
-    input = <<-CODE
+    round_trip <<-CODE
 class Foo
   def bar
     [1, 2].each do |baz|
@@ -12,6 +18,13 @@ class Foo
   end
 end
     CODE
-    refactor(input).should == input
+  end
+
+  it 'respects your funny bracketing conventions' do
+    round_trip <<-CODE
+def foo bar
+(bar + (1))
+end
+    CODE
   end
 end
