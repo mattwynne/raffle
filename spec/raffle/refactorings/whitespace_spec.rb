adea038cc2e45f382e5d13a66e4fe575dbbf6d24
Matt Wynne <matt@mattwynne.net>
2012-07-03 14:58:21 +0100

    Change @chrismdp's pending spec into a failing one.

--- spec/raffle/refactorings/whitespace_spec.rb
+++ spec/raffle/refactorings/whitespace_spec.rb
@@ -1,7 +1,17 @@
 require 'ripper_helper'
+require 'raffle/refactorings/noop'
 
-describe "Whitespace" do
-  it 'reformats all code to two spaces' do
-    pending 'this should ideally be something that sorcerer handles for us?'
+describe Raffle::Refactorings::Noop do
+  it 'reformats all code to two space indentation' do
+    input = <<-CODE
+class Foo
+  def bar
+    [1, 2].each do |baz|
+      puts baz
+    end
+  end
+end
+    CODE
+    refactor(input).should == input
   end
 end
