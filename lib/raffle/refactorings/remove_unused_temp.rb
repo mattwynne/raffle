108f8554ef315d9e727f51ebbf8f54d3405ecfbf
Stuart Gale <bishboria@gmail.com>
2012-07-03 21:37:28 +0100

    When the temp is used, s-expression is unchanged

--- lib/raffle/refactorings/remove_unused_temp.rb
+++ lib/raffle/refactorings/remove_unused_temp.rb
@@ -7,9 +7,15 @@ module Raffle
       include NavigatesTrees
 
       def call(starting_sexp, temp_name)
+        return starting_sexp if more_than_one_use_of?(starting_sexp, temp_name)
+
         transform(starting_sexp) do |sexp|
           sexp.reject { |child| assignment_with_name?(child, temp_name) }
         end
+      end
+
+      def more_than_one_use_of?(starting_sexp, temp_name)
+        starting_sexp.flatten.count { |element| element == temp_name } > 1
       end
     end
   end
