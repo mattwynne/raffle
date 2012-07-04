78302327427ed4aed803d4114c96afb29e6302c2
Chris Parsons <chris@thinkcodelearn.com>
2012-07-04 06:50:32 +0100

    Rename core transform method to express intent

--- lib/raffle/refactorings/navigates_trees.rb
+++ lib/raffle/refactorings/navigates_trees.rb
@@ -13,23 +13,23 @@ module Raffle
         return current_sexp unless current_sexp.respond_to?(:map)
         current_sexp.map do |child_sexp|
           if (child_sexp == scope_sexp)
-            transform_no_scope_boundary_change(child_sexp, scope_proc, &block)
+            transform_respecting_scope_changes(child_sexp, scope_proc, &block)
           else
             transform_within_scope(child_sexp, scope_sexp, scope_proc, &block)
           end
         end
       end
 
-      def transform_no_scope_boundary_change(sexp, scope_proc, &block)
+      def transform_respecting_scope_changes(sexp, scope_proc, &block)
         return sexp unless sexp.respond_to?(:map)
         return sexp if scoping_delimiter?(sexp) && !scope_proc.call(sexp)
         (block.call(sexp) || sexp).map do |child|
-          transform_no_scope_boundary_change(child, scope_proc, &block)
+          transform_respecting_scope_changes(child, scope_proc, &block)
         end
       end
 
       def transform(sexp, &block)
-        transform_no_scope_boundary_change(sexp, lambda { |x| true }, &block)
+        transform_respecting_scope_changes(sexp, lambda { |x| true }, &block)
       end
 
       def find_last(sexp, &block)
