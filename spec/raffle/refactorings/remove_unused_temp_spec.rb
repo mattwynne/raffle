efb9d50286f5fcf7d283d74d6be46900dc76eab6
Matt Wynne <matt@mattwynne.net>
2012-07-05 12:58:34 +0100

    Add another failing spec.
    @bishboria this should help drive out tightening up the search for
    usages :)

--- spec/raffle/refactorings/remove_unused_temp_spec.rb
+++ spec/raffle/refactorings/remove_unused_temp_spec.rb
@@ -2,26 +2,26 @@ require 'ripper_helper'
 require_relative '../../../lib/raffle/refactorings/remove_unused_temp'
 
 describe Raffle::Refactorings::RemoveUnusedTemp do
-  let(:input) do
-    %{
+  context 'when the temp is not used anywhere else in the same method' do
+    let(:input) { <<-CODE }
       def thing
         fred = 35
         42
       end
-    }
-  end
+    CODE
 
-  it 'returns an s-expression with the temp removed' do
-    refactor(input, 'fred').should == "def thing\n42\nend"
-  end
+    it 'removes the temp' do
+      refactor(input, 'fred').should == "def thing\n42\nend"
+    end
 
-  context 'when the temp is not found' do
-    it 'returns the s-expression unchanged' do
-      refactor(input, 'jim').should == "def thing\nfred = 35\n42\nend"
+    context 'when the temp is not found' do
+      it 'returns the s-expression unchanged' do
+        refactor(input, 'jim').should == "def thing\nfred = 35\n42\nend"
+      end
     end
   end
 
-  context 'when the temp is used' do
+  context 'when the temp is used within the same method scope' do
     it 'returns the s-expression unchanged' do
       input = %{
         def thing
@@ -33,6 +33,31 @@ describe Raffle::Refactorings::RemoveUnusedTemp do
 
       refactor(input, 'fred').should ==
         "def thing\nfred = 35\ndo_something_to fred\n42\nend"
+    end
+  end
+
+  context "when the temp's name is used as the name of another method" do
+    let(:input) { <<-CODE }
+      def thing
+        fred = 35
+        42
+      end
+
+      def fred
+        "something unrelated"
+      end
+    CODE
+
+    it 'removes the temp from the first method' do
+      refactor(input, 'fred').should == <<-CODE
+def thing
+42
+end
+
+def fred
+"something unrelated"
+end
+      CODE
     end
   end
 
