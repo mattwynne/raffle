3e208aa5f782e1ecc3cbfd50fc0df491c956ff3c
Matt Wynne <matt@mattwynne.net>
2012-07-11 22:31:26 +0100

    Replace feature for remove unused temp with one for rename temp
    This is our best refactoring, in that it has some awareness of position.
    
    It still needs to be better though, because it doens't need to ask me
    for the original name - it should be able to work it out from the
    position.

---
+++ features/rename_temp.feature
@@ -0,0 +1,26 @@
+Feature: Rename temp
+
+  This refactoring replaces all uses of a given temporary
+  variable with a new name.
+
+  Background:
+    Given a file `lib/foo.rb` with:
+      """
+      def name
+        fred = "fred"
+        "My name is " + fred
+      end
+
+      """
+
+  Scenario: Rename temp by pointing to it
+    When I run `raffle RenameTemp lib/foo.rb:3,0 fred name`
+    Then the file `lib/foo.rb` should contain:
+      """
+      def name
+        name = "fred"
+        "My name is " + name
+      end
+
+      """
+
--- lib/raffle/cli.rb
+++ lib/raffle/cli.rb
@@ -4,12 +4,12 @@ require 'sorcerer'
 module Raffle
   class CLI < Struct.new(:file_system)
 
-    def run(refactoring_name, *args)
+    def run(refactoring_name, *raw_args)
+      args = Arguments.parse(raw_args)
+      sexp = convert(file_system.read(args.file))
       refactoring = Refactorings.find(refactoring_name)
-      file = args.shift
-      sexp = convert(file_system.read(file))
-      result = refactoring.call(sexp, *args)
-      file_system.write(file, rubify(result))
+      result = refactoring.call(sexp, args.start_position, *args.rest)
+      file_system.write(args.file, rubify(result))
     end
 
     def convert(source)
@@ -18,6 +18,15 @@ module Raffle
 
     def rubify(sexpr)
       Sorcerer.source(sexpr, multiline: true, indent: true)
+    end
+
+    class Arguments < Struct.new(:file, :start_position, :rest)
+      def self.parse(raw_args)
+        location = raw_args.shift
+        file, raw_range = location.split(':')
+        start_position = Position.new(raw_range.split(',').map(&:to_i))
+        new(file, start_position, raw_args)
+      end
     end
   end
 end
